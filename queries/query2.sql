222a64f26383e9c57f692d2dec7f4778aaa16182
Zulfah Adams <zulfahadms@gmail.com>
2026-06-05 23:38:51 +0200

    query2.sql

--- queries/query2.sql
+++ queries/query2.sql
@@ -1,7 +1,11 @@
+
+
+
+
 SELECT 
     order_status,
     COUNT(*) AS order_count,
-    ROUND(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER(), 2) AS percentage
+    ROUND(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER (), 2) AS pct_of_all_orders
 FROM orders
 GROUP BY order_status
 ORDER BY order_count DESC;
